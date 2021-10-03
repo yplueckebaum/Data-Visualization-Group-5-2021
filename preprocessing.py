0c395d0fcfee8c33d434c7098af47f3992410dec
Typenstein <62467994+Typenstein@users.noreply.github.com>
2021-10-03 22:24:50 +0200

    Create complete dataset by running create_database notebook. Csv and json files have to be in the Dataset folder

---
+++ preprocessing.py
@@ -0,0 +1,57 @@
+# %%
+import json
+import time
+import datetime
+import numpy as np
+import os
+import glob
+import seaborn as sb
+import matplotlib as plt
+import pandas as pd
+
+
+class Preprocessing:
+
+    def __init__(self,csv_path, json_path):
+        self.df = pd.read_csv(csv_path)
+        self.region = json_path.split('/')[1][:2]
+        self.csv_path = csv_path
+        self.json_path = json_path
+
+    def process_region_data(self):
+        print(self.region)
+        self.df.replace("[None]", np.nan, inplace=True)
+        # Add new columns:
+        self._add_category_string(self.json_path)
+        self._add_tagged_column()
+        self._add_region_column()
+        return self.df
+
+    def _add_category_string(self, json_path):
+        category_file = open(json_path)
+        category_file = json.load(category_file)
+        category_list = [0 for i in range(0, 45)]
+        for elem in category_file['items']:
+            category_list[int(elem['id'])] = elem['snippet']['title']
+        # generate column that fits category from dataframe
+        category_text_column = []
+        for item in self.df['categoryId']:
+            category_text_column.append(category_list[item])
+        # add categories to dataframe
+        self.df['category_text'] = category_text_column
+
+    def _add_tagged_column(self):
+        tagged = []
+        for tag in self.df['tags']:
+            if tag == "[None]":
+                tagged.append(False)
+            else:
+                tagged.append(True)
+        self.df['tagged'] = tagged
+        df_print = pd.DataFrame(self.df, columns=['tags', 'tagged'])
+
+    def _add_region_column(self):
+        region_column = []
+        for i in range(self.df.shape[0]):
+            region_column.append(self.region)
+        self.df['region'] = region_column
