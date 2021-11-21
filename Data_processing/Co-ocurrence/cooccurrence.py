434eb2f8ae5d2b8cf53c5557fd2da0d18a4a9dc9
Yasar Laurin Plückebaum <yasar.plueckebaum@uni-bielefeld.de>
2021-11-21 12:51:57 +0100

    small create Dataset fix. Dataset cant have any other csv files

--- Data_processing/Co-ocurrence/cooccurrence.py
+++ Data_processing/Co-ocurrence/cooccurrence.py
@@ -29,7 +29,7 @@ class CoOccurrence:
         self.partition = None
 
     def setup(self,csv_path: str = "."):
-        self.df = pd.read_csv(csv_path + "/processed_dataset.csv")
+        self.df = pd.read_csv(csv_path + "/processed_dataset.csv",engine="python",error_bad_lines=False)
         self.data_len = self.df.shape[0]
         unique_tags = []
         for index, row in self.df.iterrows():
