c0e4e2533c1907a74efbd08e87e039ae5b675e4b
TobiasDJ <tobiasdammhenrichsen@gmail.com>
2021-11-21 11:52:31 +0100

    added stacked area chart

--- dashboard.py
+++ dashboard.py
@@ -11,7 +11,38 @@ from dash import html
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output, State
 
-import plotly.express as px
+import plotly.graph_objects as go
+import pandas as pd
+
+
+## Align what colours to use and correct scale
+available_colors = ["#a6cee3","#1f78b4","#b2df8a", "#33a02c","#fb9a99","#e31a1c","#fdbf6f","#ff7f00","#cab2d6","#6a3d9a","#ffff99","#b15928"]
+
+processed_csv_path = r"C:\Users\Tobias\Desktop\Data visualization\Project\Data-Visualization-Group-5-2021/processed_dataset.csv"
+
+def prepareData(processed_csv_path):
+    
+    df = pd.read_csv(processed_csv_path)
+    df.publishedAt= pd.to_datetime(df.publishedAt)
+    df.trending_date= pd.to_datetime(df.trending_date)
+    
+    df['Years'] = df['trending_date'].dt.strftime('%Y')
+    df['Months'] = df['trending_date'].dt.strftime('%Y-%m')
+    df['Days'] = df['trending_date'].dt.date
+
+    return df
+
+def extractFilterOptions(df):
+    available_regions = df['region'].unique()
+    available_categories = df['category_text'].unique()
+    
+    return available_regions, available_categories
+
+df = prepareData(processed_csv_path)
+available_regions, available_categories = extractFilterOptions(df)
+
+
+### DASHBOARD UI
 
 # the style arguments for the sidebar.
 SIDEBAR_STYLE = {
@@ -43,85 +74,41 @@ CARD_TEXT_STYLE = {
 
 controls = dbc.FormGroup(
     [
-        html.P('Regions', style={
+        html.P('Countries', style={
             'textAlign': 'center'
         }),
         dcc.Dropdown(
-            id='dropdown',
-            options=[{
-                'label': 'Value One',
-                'value': 'value1'
-            }, {
-                'label': 'Value Two',
-                'value': 'value2'
-            },
-                {
-                    'label': 'Value Three',
-                    'value': 'value3'
-                }
-            ],
-            value=['value1'],  # default value
-            multi=True
+            id='display_regions',
+            options=[{'label': i, 'value': i} for i in available_regions],
+            value='GB',  # default value
         ),
         html.P('Categories', style={
             'textAlign': 'center'
         }),
-        dbc.Card([dbc.Checklist(
-            id='check_list',
-            options=[{
-                'label': 'Value One',
-                'value': 'value1'
-            },
-                {
-                    'label': 'Value Two',
-                    'value': 'value2'
-                },
-                {
-                    'label': 'Value Three',
-                    'value': 'value3'
-                }
-            ],
-            value=['value1', 'value2'],
-            inline=True
+        dbc.Card([dcc.Dropdown(
+            id='display_categories',
+            options=[{'label': i, 'value': i} for i in available_categories],
+            value=["Music","Gaming"], # default value
+            multi=True,
         )]),
         html.Br(),
-        html.P('Radio Items', style={
+        html.P('Time scale', style={
             'textAlign': 'center'
         }),
         dbc.Card([dbc.RadioItems(
-            id='radio_items',
-            options=[{
-                'label': 'Value One',
-                'value': 'value1'
-            },
-                {
-                    'label': 'Value Two',
-                    'value': 'value2'
-                },
-                {
-                    'label': 'Value Three',
-                    'value': 'value3'
-                }
-            ],
-            value='value1',
+            id='filter_time',
+            options=[{'label': i, 'value': i} for i in ['Days', 'Months', 'Years']],
+            value='Months', # default value
             style={
                 'margin': 'auto'
             }
         )]),
-        html.Br(),
-        dbc.Button(
-            id='submit_button',
-            n_clicks=0,
-            children='Submit',
-            color='primary',
-            block=True
-        ),
     ]
 )
 
 sidebar = html.Div(
     [
-        html.H2('Parameters', style=TEXT_STYLE),
+        html.H2('Options', style=TEXT_STYLE),
         html.Hr(),
         controls
     ],
@@ -145,7 +132,7 @@ content_first_row = dbc.Row(
 content_third_row = dbc.Row(
     [
         dbc.Col(
-            dcc.Graph(id='graph_4'), md=12,
+            dcc.Graph(id='stacked-area-chart'), md=4,
         )
     ]
 )
@@ -175,17 +162,16 @@ content = html.Div(
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 app.layout = html.Div([sidebar, content])
 
-
+"""
 @app.callback(
     Output('graph_1', 'figure'),
     [Input('submit_button', 'n_clicks')],
-    [State('dropdown', 'value'), State('range_slider', 'value'), State('check_list', 'value'),
+    [State('dropdown', 'value'), State('check_list', 'value'),
      State('radio_items', 'value')
      ])
-def update_graph_1(n_clicks, dropdown_value, range_slider_value, check_list_value, radio_items_value):
+def update_graph_1(n_clicks, dropdown_value, check_list_value, radio_items_value):
     print(n_clicks)
     print(dropdown_value)
-    print(range_slider_value)
     print(check_list_value)
     print(radio_items_value)
     fig = {
@@ -195,130 +181,70 @@ def update_graph_1(n_clicks, dropdown_value, range_slider_value, check_list_valu
         }]
     }
     return fig
+"""
 
 
 @app.callback(
-    Output('graph_2', 'figure'),
-    [Input('submit_button', 'n_clicks')],
-    [State('dropdown', 'value'), State('range_slider', 'value'), State('check_list', 'value'),
-     State('radio_items', 'value')
-     ])
-def update_graph_2(n_clicks, dropdown_value, range_slider_value, check_list_value, radio_items_value):
-    print(n_clicks)
-    print(dropdown_value)
-    print(range_slider_value)
-    print(check_list_value)
-    print(radio_items_value)
-    fig = {
-        'data': [{
-            'x': [1, 2, 3],
-            'y': [3, 4, 5],
-            'type': 'bar'
-        }]
-    }
+    Output('stacked-area-chart', 'figure'),
+    [Input('display_regions', 'value'),
+     Input('filter_time', 'value'),
+     Input('display_categories', 'value')])
+def update_stacked_area_chart(regionInput, timeInput, categoryInput):
+
+    selected_region = regionInput
+    selected_time_format = timeInput
+    selected_categories = categoryInput
+    
+    fig = go.Figure()
+    color_count = 0
+    for categories in selected_categories:
+        
+        videos_that_match = df.loc[(df.category_text==categories) & (df.region == selected_region),selected_time_format]
+        videos_that_match_count = videos_that_match.value_counts()
+        videos_that_match_count = videos_that_match_count.sort_index()
+        
+        x = videos_that_match_count.index
+        y = videos_that_match_count.values
+        
+        ## example on filter on time
+        ##x=df.loc[(df.category_text==categories) & (df.region == selected_region),selected_time_format],
+        ##y=df.loc[(df.category_text==categories) & (df.region == selected_region),selected_metric],
+            
+        
+        color_count += 1
+        fig.add_trace(go.Scatter(
+            x = x,
+            y = y,
+            mode='lines',
+            name=categories,
+            line=dict(width=0.5, color=available_colors[color_count-1]),
+            stackgroup='one', # define stack group
+            groupnorm='percent' # sets the normalization for the sum of the stackgroup
+        ))        
+
+
+    fig.update_layout(
+     title = selected_region + ": trending YouTube data",
+     title_font_size = 20, legend_font_size = 10,
+     showlegend=True,
+     width = 800, height = 700,
+     yaxis=dict(type='linear',ticksuffix='%'))
+
+    fig.update_xaxes(
+         title_text = 'Date',
+         title_font=dict(size=15, family='Verdana', color='black'),
+         tickfont=dict(family='Calibri', color='black', size=12))
+
+    fig.update_yaxes(
+         title_text = "Number of videos(%)", range = (0,100),
+         title_font=dict(size=15, family='Verdana', color='black'),
+         tickfont=dict(family='Calibri', color='black', size=12))
+
     return fig
 
 
-@app.callback(
-    Output('graph_3', 'figure'),
-    [Input('submit_button', 'n_clicks')],
-    [State('dropdown', 'value'), State('range_slider', 'value'), State('check_list', 'value'),
-     State('radio_items', 'value')
-     ])
-def update_graph_3(n_clicks, dropdown_value, range_slider_value, check_list_value, radio_items_value):
-    print(n_clicks)
-    print(dropdown_value)
-    print(range_slider_value)
-    print(check_list_value)
-    print(radio_items_value)
-    df = px.data.iris()
-    fig = px.density_contour(df, x='sepal_width', y='sepal_length')
-    return fig
 
 
-@app.callback(
-    Output('graph_4', 'figure'),
-    [Input('submit_button', 'n_clicks')],
-    [State('dropdown', 'value'), State('range_slider', 'value'), State('check_list', 'value'),
-     State('radio_items', 'value')
-     ])
-def update_graph_4(n_clicks, dropdown_value, range_slider_value, check_list_value, radio_items_value):
-    print(n_clicks)
-    print(dropdown_value)
-    print(range_slider_value)
-    print(check_list_value)
-    print(radio_items_value)  # Sample data and figure
-    df = px.data.gapminder().query('year==2007')
-    fig = px.scatter_geo(df, locations='iso_alpha', color='continent',
-                         hover_name='country', size='pop', projection='natural earth')
-    fig.update_layout({
-        'height': 600
-    })
-    return fig
-
-
-@app.callback(
-    Output('graph_5', 'figure'),
-    [Input('submit_button', 'n_clicks')],
-    [State('dropdown', 'value'), State('range_slider', 'value'), State('check_list', 'value'),
-     State('radio_items', 'value')
-     ])
-def update_graph_5(n_clicks, dropdown_value, range_slider_value, check_list_value, radio_items_value):
-    print(n_clicks)
-    print(dropdown_value)
-    print(range_slider_value)
-    print(check_list_value)
-    print(radio_items_value)  # Sample data and figure
-    df = px.data.iris()
-    fig = px.scatter(df, x='sepal_width', y='sepal_length')
-    return fig
-
-
-@app.callback(
-    Output('graph_6', 'figure'),
-    [Input('submit_button', 'n_clicks')],
-    [State('dropdown', 'value'), State('range_slider', 'value'), State('check_list', 'value'),
-     State('radio_items', 'value')
-     ])
-def update_graph_6(n_clicks, dropdown_value, range_slider_value, check_list_value, radio_items_value):
-    print(n_clicks)
-    print(dropdown_value)
-    print(range_slider_value)
-    print(check_list_value)
-    print(radio_items_value)  # Sample data and figure
-    df = px.data.tips()
-    fig = px.bar(df, x='total_bill', y='day', orientation='h')
-    return fig
-
-
-@app.callback(
-    Output('card_title_1', 'children'),
-    [Input('submit_button', 'n_clicks')],
-    [State('dropdown', 'value'), State('range_slider', 'value'), State('check_list', 'value'),
-     State('radio_items', 'value')
-     ])
-def update_card_title_1(n_clicks, dropdown_value, range_slider_value, check_list_value, radio_items_value):
-    print(n_clicks)
-    print(dropdown_value)
-    print(range_slider_value)
-    print(check_list_value)
-    print(radio_items_value)  # Sample data and figure
-    return 'Card Tile 1 change by call back'
-
-
-@app.callback(
-    Output('card_text_1', 'children'),
-    [Input('submit_button', 'n_clicks')],
-    [State('dropdown', 'value'), State('range_slider', 'value'), State('check_list', 'value'),
-     State('radio_items', 'value')
-     ])
-def update_card_text_1(n_clicks, dropdown_value, range_slider_value, check_list_value, radio_items_value):
-    print(n_clicks)
-    print(dropdown_value)
-    print(range_slider_value)
-    print(check_list_value)
-    print(radio_items_value)  # Sample data and figure
-    return 'Card text change by call back'
 
 
 if __name__ == '__main__':
