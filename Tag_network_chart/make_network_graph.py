fafb427c9b9e57a24004ad36a30a467ba451d5ef
Yasar Laurin Plückebaum <yasar.plueckebaum@uni-bielefeld.de>
2021-11-29 17:30:13 +0100

    Network graph check list. Tried out random init for the layout.
    Implemented edge filtering based on node degree
    
    tried(and failed) to impkement scattergl for performance.

--- Tag_network_chart/make_network_graph.py
+++ Tag_network_chart/make_network_graph.py
@@ -7,20 +7,40 @@ from Data_processing.Coocurrence.cooccurrence import CoOccurrence
 
 
 # todo: This should support figure import/export using something!
-def make_network_fig(graph,cooccurrence : CoOccurrence,edge_weight_min = 5):
+def make_network_fig(graph, cooccurrence: CoOccurrence, edge_degree_min=5):
     # import graph
-    test = cooccurrence.normalize_by_occurrence()
-    G = graph#nx.random_geometric_graph(200, 0.125)
+    G = graph  # nx.random_geometric_graph(200, 0.125)
     # decide whether or not to plot data points or clusters
-    layout = nx.drawing.nx_agraph.pygraphviz_layout(G,prog="sfdp")
-    print("test")
+    layout = nx.drawing.layout.kamada_kawai_layout(G=G, pos={i: r for i, r in zip(range(G.number_of_nodes()), list(
+        nx.drawing.layout.random_layout(G).values()))})  # nx.drawing.nx_agraph.pygraphviz_layout(G,prog="sfdp")
     # in case of edges generate x and y of edges
     # None acts as seperator
+
+    # safe x and y of nodes
+    node_x = []
+    node_y = []
+    sizes = []
+    node_text = []
+    size_dict = cooccurrence.normalize_by_occurrence()
+    edge_nodes = []
+    node_degree = []
+    for node in G.nodes():
+        x, y = layout[node]
+        node_x.append(x)
+        node_y.append(y)
+        sizes.append(size_dict[cooccurrence.tags_dict_inverse[node]] * 10)
+        node_text.append(cooccurrence.tags_dict_inverse[node])
+        if G.degree(node) >= edge_degree_min:
+            edge_nodes.append(True)
+        else:
+            edge_nodes.append(False)
+        node_degree.append(G.degree(node))
+
     edge_x = []
     edge_y = []
 
     for edge in G.edges.data("weight"):
-        if cooccurrence.tag_graph.get_edge_data(*edge)["weight"] >= edge_weight_min:
+        if edge_nodes[edge[0]] or edge_nodes[edge[1]]:
             x0, y0 = layout[edge[0]]
             x1, y1 = layout[edge[1]]
             edge_x.append(x0)
@@ -35,20 +55,11 @@ def make_network_fig(graph,cooccurrence : CoOccurrence,edge_weight_min = 5):
         hoverinfo='text',
         mode='lines')
 
-    # safe x and y of nodes
-    node_x = []
-    node_y = []
-    sizes = []
-    size_dict = cooccurrence.normalize_by_occurrence()
-    for node in G.nodes():
-        x, y = layout[node]
-        node_x.append(x)
-        node_y.append(y)
-        sizes.append(size_dict[cooccurrence.tags_dict_inverse[node]] * 10)
-
     node_trace = go.Scatter(
         x=node_x, y=node_y,
-        mode='markers',
+        mode='text+markers',
+        text=node_text,
+        textposition="top center",
         hoverinfo='text',
         marker=dict(
             showscale=True,
@@ -59,7 +70,7 @@ def make_network_fig(graph,cooccurrence : CoOccurrence,edge_weight_min = 5):
             colorscale='YlGnBu',
             reversescale=True,
             color=[],
-            size=10,#sizes
+            size=10,  # sizes
             colorbar=dict(
                 thickness=15,
                 title='Node Connections',
